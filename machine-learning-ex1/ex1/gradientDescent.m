972809a7dc435a98b21ea753a46c97e2c5dfa712
yousef <srap5664@gmail.com>
2019-06-08 09:10:42 +0300

    Gradiant Descent Univariate

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -17,13 +17,9 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
+theta = theta - alpha * (1/m) * (X' * ((X * theta) - y)); 
 
-
-
-
-
-
-    % ============================================================
+% ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
